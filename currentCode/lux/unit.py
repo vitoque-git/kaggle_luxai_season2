62f376cbaccf9a5dbb249dced4d1a64c35080f44
vito <vito.imburgia@quodfinancial.com>
2023-03-23 18:30:54 +0100

    NF Episode: Final Scores: {'player_0': 11511, 'player_1': 7582}

--- currentCode/lux/unit.py
+++ currentCode/lux/unit.py
@@ -9,11 +9,12 @@ from lux.config import EnvConfig
 # a[1] = direction (0 = center, 1 = up, 2 = right, 3 = down, 4 = left)
 move_deltas = np.array([[0, 0], [0, -1], [1, 0], [0, 1], [-1, 0]])
 
+
 @dataclass
 class Unit:
     team_id: int
     unit_id: str
-    unit_type: str # "LIGHT" or "HEAVY"
+    unit_type: str  # "LIGHT" or "HEAVY"
     pos: np.ndarray
     power: int
     cargo: UnitCargo
@@ -22,16 +23,17 @@ class Unit:
     action_queue: List
 
     def unit_type_short(self):
-        if len(self.unit_type)>0:
+        if len(self.unit_type) > 0:
             return self.unit_type[0]
         else:
             return ''
 
     def unit_id_short(self):
         if len(self.unit_type) > 4:
-            return 'u'+self.unit_id[4:]
+            return 'u' + self.unit_id[4:]
         else:
             return self.unit_id
+
     @property
     def agent_id(self):
         if self.team_id == 0: return "player_0"
@@ -54,11 +56,11 @@ class Unit:
             # print("Warning, tried to get move cost for going onto a opposition factory", file=sys.stderr)
             return None
         rubble_at_target = board.rubble[target_pos[0]][target_pos[1]]
-        
+
         return math.floor(self.unit_cfg.MOVE_COST + self.unit_cfg.RUBBLE_MOVEMENT_COST * rubble_at_target)
 
     def move_cost(self, game_state, direction):
-        return self.move_cost_to(game_state,self.pos + move_deltas[direction])
+        return self.move_cost_to(game_state, self.pos + move_deltas[direction])
 
     def move(self, direction, repeat=0, n=1):
         if isinstance(direction, int):
@@ -71,18 +73,20 @@ class Unit:
         assert transfer_resource < 5 and transfer_resource >= 0
         assert transfer_direction < 5 and transfer_direction >= 0
         return np.array([1, transfer_direction, transfer_resource, transfer_amount, repeat, n])
-    
+
     def pickup(self, pickup_resource, pickup_amount, repeat=0, n=1):
         assert pickup_resource < 5 and pickup_resource >= 0
         return np.array([2, 0, pickup_resource, pickup_amount, repeat, n])
-    
+
     def dig_cost(self):
         return self.unit_cfg.DIG_COST
+
     def dig(self, repeat=0, n=1):
         return np.array([3, 0, 0, 0, repeat, n])
 
     def self_destruct_cost(self, game_state):
         return self.unit_cfg.SELF_DESTRUCT_COST
+
     def self_destruct(self, repeat=0, n=1):
         return np.array([4, 0, 0, 0, repeat, n])
 
@@ -96,7 +100,7 @@ class Unit:
         return self.battery_capacity() - self.power
 
     def battery_info(self):
-        return "battery("+str(self.power)+"/"+str(self.battery_capacity())+" lft="+str(self.battery_capacity_left())+")"
+        return "battery(" + str(self.power) + "/" + str(self.battery_capacity()) + " lft=" + str(self.battery_capacity_left()) + ")"
 
     def cargo_space(self):
         return 100 if self.unit_type == "LIGHT" else 1000
@@ -104,18 +108,24 @@ class Unit:
     def def_move_cost(self):
         return 1 if self.unit_type == "LIGHT" else 20
 
-    def rubble_dig_hurdle(self): #SEEMS SOMETHING SPECIFIC
+    def rubble_dig_hurdle(self):  # SEEMS SOMETHING SPECIFIC
         return 5 if self.unit_type == "LIGHT" else 100
 
     def pos_location(self):
-        return (self.pos[0],self.pos[1])
+        return (self.pos[0], self.pos[1])
 
-    def get_distance(self,  pos):
-        return abs(self.pos[0]-pos[0]) + abs(self.pos[1]-pos[1])
+    def get_distance(self, pos):
+        return abs(self.pos[0] - pos[0]) + abs(self.pos[1] - pos[1])
 
     # Manhattan Distance between one points and one vector, return a vector
     def get_distance_vector(self, points):
         return 2 * np.mean(np.abs(points - self.pos), 1)
+
+    def is_heavy(self):
+        return self.unit_type == 'HEAVY'
+
+    def is_light(self):
+        return self.unit_type == 'LIGHT'
 
     def __str__(self) -> str:
         out = f"[{self.team_id}] {self.unit_id} {self.unit_type} at {self.pos}"
